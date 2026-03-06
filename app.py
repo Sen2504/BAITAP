4bc0e3be42b84af4d37bdd481936081d324e6c9d
BAITAP <minhsang5544@gmail.com>
2026-03-06 11:20:00 +0700

    change app.run

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import io
 import base64
+import os
 
 app = Flask(__name__)
 
@@ -156,6 +157,6 @@ def index():
 
     return render_template("index.html", table=table_html, chart_url=chart_url)
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
